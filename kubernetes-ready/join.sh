b3badad25d1e7dc60b1637b0a98e8f87abc6aeb6
ayoubensalem <ayoubensalem@gmail.com>
2020-08-14 19:55:07 +0100

    updates

--- kubernetes-ready/join.sh
+++
@@ -1,2 +0,0 @@
-kubeadm join 10.0.0.10:6443 --token xm4bdl.14riwwk9q28o0a1n \
-    --discovery-token-ca-cert-hash sha256:544d0f758abe3e67c7a822b204eb040309441358d92e9a3ee0ec8d1cc384b32b 
---
+++ kubernetes-ready/readme.md
@@ -0,0 +1,1 @@
+Credits to : https://github.com/LocusInnovations/k8s-vagrant-virtualbox
